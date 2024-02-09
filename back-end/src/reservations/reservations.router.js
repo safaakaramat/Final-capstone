b56cd535241ca7b45490c6cfdc1bca8ba07176fc
Safaa <karamatsafaa0@gmail.com>
2024-02-09 14:30:50 -0700

    Initial commit

---
+++ back-end/src/reservations/reservations.router.js
@@ -0,0 +1,34 @@
+/**
+ * Defines the router for reservation resources.
+ *
+ * @type {Router}
+ */
+
+ const router = require("express").Router();
+ const controller = require("./reservations.controller");
+ const methodNotAllowed = require('../errors/methodNotAllowed');
+
+ router
+   .route("/")
+   .get(controller.list)
+   .post(controller.create)
+   .all(methodNotAllowed);
+
+ router
+   .route("/new")
+   .post(controller.create)
+   .all(methodNotAllowed);
+
+ router
+   .route("/:reservationId([0-9]+)")
+   .get(controller.read)
+   .put(controller.updateReservation)
+   .all(methodNotAllowed);
+
+ router
+   .route("/:reservationId([0-9]+)/status")
+   .put(controller.updateStatus)
+   .all(methodNotAllowed);
+
+
+ module.exports = router;
