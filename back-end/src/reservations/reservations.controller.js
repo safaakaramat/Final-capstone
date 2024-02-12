fda543743e7c0c31bff96920ed2ae3226d58506c
Safaa <karamatsafaa0@gmail.com>
2024-02-11 18:49:24 -0700

    remove current date

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -198,13 +198,10 @@
   * List handler for reservation resources
   */
  async function list(req, res) {
-   const { date, currentDate, mobile_number } = req.query;
+   const { date, mobile_number } = req.query;
    if (date) {
      const data = await service.listByDate(date);
      res.json({ data: data });
-   } else if (currentDate) {
-     const data = await service.listByDate(currentDate);
-     res.json({ data: data  });
    } else if (mobile_number) {
      const data = await service.listByPhone(mobile_number);
      res.json({ data: data  });
